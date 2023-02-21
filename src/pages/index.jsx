69c9b66220151fbaa26c490d166e67a9128141b6
heyfranksmile <heyfranksmile@gmail.com>
2023-02-21 15:31:00 +0900

    feat: pages setup

---
+++ src/pages/index.jsx
@@ -0,0 +1,35 @@
+import Calendar from "./Calendar";
+import ColorPicker from "./ColorPicker";
+import Customers from "./Customers";
+import Ecommerce from "./Ecommerce";
+import Kanban from "./Kanban";
+import Orders from "./Orders";
+import Editor from "./Editor";
+import Employees from "./Employees";
+import Area from "./Charts/Area";
+import Bar from "./Charts/Bar";
+import ColorMapping from "./Charts/ColorMapping";
+import Financial from "./Charts/Financial";
+import Line from "./Charts/Line";
+import Pie from "./Charts/Pie";
+import Pyramid from "./Charts/Pyramid";
+import Stacked from "./Charts/Stacked";
+
+export {
+  Calendar,
+  ColorPicker,
+  Customers,
+  Ecommerce,
+  Kanban,
+  Orders,
+  Editor,
+  Employees,
+  Area,
+  Bar,
+  ColorMapping,
+  Financial,
+  Line,
+  Pie,
+  Pyramid,
+  Stacked,
+};
