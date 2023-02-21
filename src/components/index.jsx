12ca7d6b9fc0c3d1a6b213382ee12739942c0ee0
heyfranksmile <heyfranksmile@gmail.com>
2023-02-21 15:06:24 +0900

    feat: set up components

---
+++ src/components/index.jsx
@@ -0,0 +1,49 @@
+import Button from "./Button";
+import ThemeSettings from "./Button";
+import Sidebar from "./Button";
+import Navbar from "./Button";
+import Footer from "./Button";
+import Cart from "./Button";
+import Chat from "./Button";
+import Notification from "./Button";
+import UserProfile from "./Button";
+import SparkLine from "./Button";
+import LineChart from "./Button";
+import Stacked from "./Button";
+import Pie from "./Button";
+import ChartsHeader from "./Button";
+import Header from "./Button";
+
+export {
+  Button,
+  ThemeSettings,
+  Sidebar,
+  Navbar,
+  Footer,
+  Cart,
+  Chat,
+  Notification,
+  UserProfile,
+  SparkLine,
+  LineChart,
+  Stacked,
+  Pie,
+  ChartsHeader,
+  Header,
+};
+
+// export { default as Button } from "./Button";
+// export { default as ThemeSettings } from "./ThemeSettings";
+// export { default as Sidebar } from "./Sidebar";
+// export { default as Navbar } from "./Navbar";
+// export { default as Footer } from "./Footer";
+// export { default as Cart } from "./Cart";
+// export { default as Chat } from "./Chat";
+// export { default as Notification } from "./Notification";
+// export { default as UserProfile } from "./UserProfile";
+// export { default as SparkLine } from "./Charts/SparkLine";
+// export { default as LineChart } from "./Charts/LineChart";
+// export { default as Stacked } from "./Charts/Stacked";
+// export { default as Pie } from "./Charts/Pie";
+// export { default as ChartsHeader } from "./ChartsHeader";
+// export { default as Header } from "./Header";
