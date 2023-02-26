e403cf9d1f48974be60e5ffed7c5f34b8ae3ed6f
heyfranksmile <heyfranksmile@gmail.com>
2023-02-26 20:58:56 +0900

    feat: context API setting

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
-import React from "react";
+import { useEffect } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { FiSettings } from "react-icons/fi";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+import { useStateContext } from "./contexts/ContextProvider";
 
 import "./App.css";
 
 const App = () => {
-  return <div className="underline text-3xl">App</div>;
+  const { setCurrentColor, setCurrentMode, currentColor, setThemeSettings } =
+    useStateContext();
+
+  useEffect(() => {
+    const currentThemeColor = localStorage.getItem("colorMode");
+    const currentThemeMode = localStorage.getItem("themeMode");
+
+    if (currentThemeColor && currentThemeMode) {
+      setCurrentColor(currentThemeColor);
+      setCurrentMode(currentThemeMode);
+    }
+  }, []);
+
+  return (
+    <div>
+      <BrowserRouter>
+        <div className="flex relative dark:bg-main-dark-bg">
+          <TooltipComponent content="Settings" position="Top">
+            <button
+              type="button"
+              onClick={() => setThemeSettings(true)}
+              style={{ background: currentColor, borderRadius: "50%" }}
+              className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
+            >
+              <FiSettings />
+            </button>
+          </TooltipComponent>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
